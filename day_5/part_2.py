87d03893f8dbfcbc54dcab72f46ef1ab3bd5fd8f
Nathan Cruz <nathan_cruz77@protonmail.com>
2023-12-09 19:47:25 -0700

    Day five part two

---
+++ day_5/part_2.py
@@ -0,0 +1,78 @@
+from pprint import pp
+from itertools import chain
+from itertools import zip_longest
+
+def parse_maps(lines):
+    maps = []
+    current_map = None
+
+    for line in lines:
+        # Header
+        if line.endswith('map:'):
+            continue
+
+        if not line:
+            maps.append(current_map)
+            current_map = None
+        else:
+            if current_map is None:
+                current_map = []
+
+            [dst_range_start, src_range_start, length] = [int(i) for i in line.split()]
+
+            src_range = range(src_range_start, src_range_start + length)
+            dst_range = range(dst_range_start, dst_range_start + length)
+
+            current_map.append([src_range, dst_range])
+
+
+    maps.append(current_map)
+
+    return maps
+
+
+def location_to_seed(location, maps, seeds):
+    item = location
+
+    for map in maps:
+        current_item = item
+
+        for src_range, dst_range in map:
+            if current_item in dst_range:
+                item = src_range.start + (item - dst_range.start)
+                break
+
+    for range_seed in seeds:
+        if item in range_seed:
+            return True
+
+    return False
+
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+lines = iter(lines)
+
+seeds = []
+for start, length in grouper([int(i) for i in next(lines).split(' ')[1:]], 2):
+    seeds.append(range(start, start + length))
+
+next(lines)
+maps = parse_maps(lines)
+
+found_seed = False
+location = 0
+
+while not found_seed:
+    found_seed = location_to_seed(location, maps[::-1], seeds)
+
+    if not found_seed:
+        location += 1
+
+print(location)
