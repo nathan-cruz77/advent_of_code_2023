d66aeea58ff870024fc665263de819f37da46e83
Nathan Cruz <nathan_cruz77@protonmail.com>
2023-12-15 20:19:16 -0700

    Day fifteen part one

---
+++ day_15/part_1.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def hash(s):
+    current_value = 0
+
+    for c in s:
+        current_value += ord(c)
+        current_value *= 17
+        current_value = current_value % 256
+
+    return current_value
+
+
+with open(sys.argv[1]) as file:
+    sequences = next(file).strip().split(',')
+
+print(sum(hash(s) for s in sequences))
